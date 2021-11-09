bf17b7527876121a7f7ac2a9abb2a7d97173370c
mays4 <maysalani84@yahoo.com>
2021-11-09 13:21:50 -0500

    adding employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Noor", last_name: "Ali", hourly_rate: 40)
+@store2.employees.create(first_name: "Mary", last_name: "Dived", hourly_rate: 43)
+@store3.employees.create(first_name: "Richerd", last_name: "Jorge", hourly_rate: 23)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
  
 end
 
